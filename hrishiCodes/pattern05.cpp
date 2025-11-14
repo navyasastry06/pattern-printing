54a3631245470127939ee62c89ea584b8e0daf1f
EdWinJaRviS2005 <hrishikeshpatil5002@gmail.com>
2025-11-14 19:48:56 +0530

    resolved isssues

--- hrishiCodes/pattern05.cpp
+++ hrishiCodes/pattern05.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         cout<<"*";
     }
         cout<<endl;
@@ -18,8 +17,7 @@ int main(){
         cout<<"*";
         cout<<endl;
     }
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         cout<<"*";
     }
     
--- hrishiCodes/pattern13.cpp
+++ hrishiCodes/pattern13.cpp
@@ -12,22 +12,19 @@ int main(){
         for(int j=2;j<(2*i)-1;j++){
             cout<<" ";
         }
-        if(i>1)
+        if(i>1){
             cout<<"*";
-
-        for(int j=1;j<=n-i;j++){
-            cout<<" ";
         }
-        
-        for(int j=1;j<=n-i;j++){
+        for(int j=1;j<=2*(n-i);j++){
             cout<<" ";
         }
         cout<<"*";
         for(int j=2;j<(2*i)-1;j++){
             cout<<" ";
         }
-        if(i>1)
+        if(i>1){
             cout<<"*";
+        }
         cout<<endl;
     }
     return 0;
